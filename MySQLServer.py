c0a9252e85b2ca4a7bbf4d0b856b095b1eddc81d
Eng-Hamit <hamitgadar05@gmail.com>
2025-06-29 21:22:59 +0200

    Fix: Catch mysql.connector.Error in MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -3,27 +3,22 @@ from mysql.connector import Error
 
 def create_database():
     try:
-        # Connect to MySQL server
         connection = mysql.connector.connect(
             host='localhost',
-            user='root',
-            password='Kaderdinho12'
+            user='root',       # Replace with your MySQL username
+            password='Kaderdinho12'    # Replace with your MySQL password
         )
 
         if connection.is_connected():
             cursor = connection.cursor()
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
-
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
-
     finally:
-        # Ensure the connection is closed properly
         if 'connection' in locals() and connection.is_connected():
             cursor.close()
             connection.close()
-            print("MySQL connection is closed.")
 
 if __name__ == "__main__":
     create_database()
